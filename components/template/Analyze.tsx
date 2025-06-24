2dd3a1834a31cb85bbdef198c6e83480cc20a586
T.Miura <mtaiga0213@icloud.com>
2025-06-24 23:01:40 +0900

    feat: AnalyzeコンポーネントにGitHub統計の画像をグリッドレイアウトで追加し、ユーザーの生産性時間を表示する画像を追加

--- components/template/Analyze.tsx
+++ components/template/Analyze.tsx
@@ -8,32 +8,43 @@ export default function Analyze({ user }: AnalyzeProps) {
   return (
     <div>
       <p>{user}のGitHub統計</p>
-      <div>
-        <Image
-          src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${user}&layout=compact&theme=tokyonight`}
-          alt={`${user}'s GitHub stats`}
-          width={495}
-          height={195}
-          unoptimized
-        />
-      </div>
-      <div>
-        <Image
-          src={`https://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=${user}&theme=tokyonight`}
-          alt={`${user}'s GitHub stats`}
-          width={495}
-          height={195}
-          unoptimized
-        />
-      </div>
-      <div>
-        <Image
-          src={`https://github-readme-stats.vercel.app/api?username=${user}&count_private=true&show_icons=true&theme=tokyonight`}
-          alt={`${user}'s GitHub stats`}
-          width={495}
-          height={195}
-          unoptimized
-        />
+      <div className="grid grid-cols-2 gap-4">
+        <div>
+          <Image
+            src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${user}&layout=compact&theme=tokyonight`}
+            alt={`${user}'s GitHub stats`}
+            width={495}
+            height={195}
+            unoptimized
+          />
+        </div>
+        <div>
+          <Image
+            src={`https://github-profile-summary-cards.vercel.app/api/cards/profile-details?username=${user}&theme=tokyonight`}
+            alt={`${user}'s GitHub stats`}
+            width={495}
+            height={195}
+            unoptimized
+          />
+        </div>
+        <div>
+          <Image
+            src={`https://github-readme-stats.vercel.app/api?username=${user}&count_private=true&show_icons=true&theme=tokyonight`}
+            alt={`${user}'s GitHub stats`}
+            width={495}
+            height={195}
+            unoptimized
+          />
+        </div>
+        <div>
+          <Image
+            src={`http://github-profile-summary-cards.vercel.app/api/cards/productive-time?username=${user}&theme=tokyonight&utcOffset=9`}
+            alt={`${user}'s GitHub stats`}
+            width={495}
+            height={195}
+            unoptimized
+          />
+        </div>
       </div>
     </div>
   );
