1214120e2a41df523714511cfac2814883aa5d96
T.Miura <mtaiga0213@icloud.com>
2025-06-24 22:45:34 +0900

    feat: AnalyzeコンポーネントにユーザーのGitHub言語統計を表示する画像を追加

--- components/template/Analyze.tsx
+++ components/template/Analyze.tsx
@@ -8,6 +8,15 @@ export default function Analyze({ user }: AnalyzeProps) {
   return (
     <div>
       <p>{user}のGitHub統計</p>
+      <div>
+        <Image
+          src={`https://github-readme-stats.vercel.app/api/top-langs/?username=${user}&layout=compact&theme=tokyonight`}
+          alt={`${user}'s GitHub stats`}
+          width={495}
+          height={195}
+          unoptimized
+        />
+      </div>
       <Image
         src={`https://github-readme-stats.vercel.app/api?username=${user}&count_private=true&show_icons=true&theme=tokyonight`}
         alt={`${user}'s GitHub stats`}
