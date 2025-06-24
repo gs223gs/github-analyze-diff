6018ba69682c6701bd0301ad5d906bfaf62373ce
T.Miura <mtaiga0213@icloud.com>
2025-06-24 21:48:58 +0900

    shadcn の導入

--- app/status/page.tsx
+++ app/status/page.tsx
@@ -1,4 +1,18 @@
 "use client";
+import { useAtom } from "jotai";
+import { userAtom } from "../state/atoms/userAtom";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
 export default function Status() {
-  return <div>Status</div>;
+  const [user, setUser] = useAtom(userAtom);
+  const [userInput, setUserInput] = useState("");
+  return (
+    <div>
+      <Input value={userInput} onChange={(e) => setUserInput(e.target.value)} />
+      <Button onClick={() => setUser(userInput)}>Set User</Button>
+      <p>{user}</p>
+    </div>
+  );
 }
