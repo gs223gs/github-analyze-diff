7666990f76f483494c63e18fb8af502ddb17480d
T.Miura <mtaiga0213@icloud.com>
2025-06-24 22:42:14 +0900

    feat: next.config.tsの画像設定を整理し、不要な設定を削除

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'github-readme-stats.vercel.app',
-        port: '',
-        pathname: '/api/**',
+        protocol: "https",
+        hostname: "github-readme-stats.vercel.app",
+        port: "",
+        pathname: "/api/**",
       },
     ],
-    dangerouslyAllowSVG: true,
-    contentDispositionType: 'attachment',
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
   },
 };
 
