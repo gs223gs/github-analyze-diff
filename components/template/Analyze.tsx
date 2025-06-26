e1d980c388140f5d04fa8d63b0874e956ec22d8b
T.Miura <mtaiga0213@icloud.com>
2025-06-26 19:45:14 +0900

    feat: AnalyzeコンポーネントにZennおよびQiitaのユーザー統計画像を追加し、画像の構造を整理

--- components/template/Analyze.tsx
+++ components/template/Analyze.tsx
@@ -12,41 +12,77 @@ export default function Analyze({ user }: AnalyzeProps) {
     <div>
       <p>{user}のGitHub統計</p>
       <div>
-        <Image
-          src={`https://img.shields.io/github/followers/${user}?label=follow&logo=github&style=flat`}
-          alt={`${user}'s GitHub stats`}
-          width={495}
-          height={195}
-          unoptimized
-        />
+        <div>
+          <Image
+            src={`https://img.shields.io/github/followers/${user}?label=follow&logo=github&style=flat`}
+            alt={`${user}'s GitHub stats`}
+            width={495}
+            height={195}
+            unoptimized
+          />
+        </div>
+        <div>
+          <Image
+            src={`https://img.shields.io/github/stars/${user}?label=star&logo=github&style=flat`}
+            alt={`${user}'s GitHub stats`}
+            width={495}
+            height={195}
+            unoptimized
+          />
+        </div>
       </div>
+
       <div>
-        <Image
-          src={`https://img.shields.io/github/stars/${user}?label=star&logo=github&style=flat`}
-          alt={`${user}'s GitHub stats`}
-          width={495}
-          height={195}
-          unoptimized
-        />
+        <div>
+          <Image
+            src={`https://badgen.org/img/zenn/${user}/articles?style=plastic`}
+            alt={`${user}'s GitHub stats`}
+            width={495}
+            height={195}
+            unoptimized
+          />
+        </div>
+        <div>
+          <Image
+            src={`https://badgen.org/img/zenn/${user}/likes?style=plastic`}
+            alt={`${user}'s GitHub stats`}
+            width={495}
+            height={195}
+            unoptimized
+          />
+        </div>
+        <div>
+          <Image
+            src={`https://badgen.org/img/zenn/${user}/followers?style=plastic`}
+            alt={`${user}'s GitHub stats`}
+            width={495}
+            height={195}
+            unoptimized
+          />
+        </div>
       </div>
+
       <div>
-        <Image
-          src={`https://badgen.org/img/zenn/${user}/articles?style=plastic`}
-          alt={`${user}'s GitHub stats`}
-          width={495}
-          height={195}
-          unoptimized
-        />
+        <div>
+          <Image
+            src={`https://badgen.org/img/qiita/${user}/contributions?style=plastic`}
+            alt={`${user}'s GitHub stats`}
+            width={495}
+            height={195}
+            unoptimized
+          />
+        </div>
+        <div>
+          <Image
+            src={`https://badgen.org/img/qiita/${user}/articles?style=plastic`}
+            alt={`${user}'s GitHub stats`}
+            width={495}
+            height={195}
+            unoptimized
+          />
+        </div>
       </div>
-      <div>
-        <Image
-          src={`https://badgen.org/img/qiita/${user}/articles?style=plastic`}
-          alt={`${user}'s GitHub stats`}
-          width={495}
-          height={195}
-          unoptimized
-        />
-      </div>
+
       <div className="grid grid-cols-2 gap-4">
         <div>
           <Image
